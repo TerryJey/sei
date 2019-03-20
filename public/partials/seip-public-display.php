569310f680815b7dda7039b7150116d62b6b8bbf
TerryJ <alexfed81@gmail.com>
2019-03-20 20:54:34 +1100

    Initial Commit

---
+++ public/partials/seip-public-display.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * @link       https://www.htag.com.au
+ * @since      1.0.0
+ *
+ * @package    Seip
+ * @subpackage Seip/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
